02e94f0211e737ca78c854d503657a99c21ac2c2
Sam Robbins <samrobbinsgb@gmail.com>
2020-11-29 11:57:43 +0000

    Get basic loops set up

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
+const colors = require("tailwindcss/colors");
 export default function IndexPage() {
+	console.log(colors);
 	return (
 		<>
 			<Head>
@@ -9,8 +11,37 @@ export default function IndexPage() {
 					content="A template Next.js application"
 				/>
 			</Head>
-			<div>
-				<h1 className="text-center">Hello, World </h1>
+			<div className="pt-4 text-center">
+				<h1 className="text-4xl font-semibold">
+					Colour accessibility test for Tailwind CSS
+				</h1>
+				<h2 className="text-gray-600">
+					In no way associated with Tailwind Labs, apart from using
+					their product
+				</h2>
+			</div>
+			<div className="container mx-auto">
+				{Object.keys(colors)
+					.filter((color) => typeof colors[color] === "object")
+					.map((color) => (
+						<>
+							<h2
+								className="text-xl font-semibold"
+								style={{ color: colors[color][700] }}
+							>
+								{color}
+							</h2>
+							<div className="grid grid-cols-10">
+								{Object.keys(colors[color]).map((shade) => (
+									<span
+										style={{ color: colors[color][shade] }}
+									>
+										{shade}
+									</span>
+								))}
+							</div>
+						</>
+					))}
 			</div>
 		</>
 	);
